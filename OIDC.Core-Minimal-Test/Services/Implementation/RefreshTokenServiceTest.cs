53a64d79cc1a1b12f08ad1a736eeaa49b2e296f2
DLMousey <tmlovelady92@gmail.com>
2025-01-21 22:13:58 +0000

    Add initial test harness for refresh token service

---
+++ OIDC.Core-Minimal-Test/Services/Implementation/RefreshTokenServiceTest.cs
@@ -0,0 +1,78 @@
+using Microsoft.EntityFrameworkCore;
+using OIDC.Core_Minimal_Test.TestUtil;
+using OIDC.Core_Minimal.DAL;
+using OIDC.Core_Minimal.DAL.Entities;
+using OIDC.Core_Minimal.Services.Implementation;
+
+namespace OIDC.Core_Minimal_Test.Services.Implementation;
+
+[TestFixture]
+public class RefreshTokenServiceTest
+{
+    private User _testUser;
+    private RefreshToken _testRefreshToken;
+    private RefreshTokenService _refreshTokenService;
+    private OIDCCoreMinimalDbContext _context;
+    
+    [SetUp]
+    public void Setup()
+    {
+        var options = new DbContextOptionsBuilder<OIDCCoreMinimalDbContext>()
+            .UseInMemoryDatabase(Guid.NewGuid().ToString())
+            .Options;
+        
+        _context = new OIDCCoreMinimalDbContext(options);
+        _testUser = UserGenerator.GenerateFixed();
+        _testRefreshToken = RefreshTokenGenerator.GenerateFixed();
+
+        _context.Users.Add(_testUser);
+        _context.RefreshTokens.Add(_testRefreshToken);
+        _context.SaveChanges();
+        
+        _refreshTokenService = new RefreshTokenService(_context);
+    }
+
+    [Test]
+    public void CanRetrieveRefreshTokenByCode()
+    {
+        RefreshToken? refreshToken = _refreshTokenService.FindAsync(_testRefreshToken.Code).Result;
+        
+        Assert.That(refreshToken, Is.Not.Null);
+        Assert.Multiple(() =>
+        {
+            Assert.That(refreshToken.Id, Is.EqualTo(_testRefreshToken.Id));
+            Assert.That(refreshToken.Code, Is.EqualTo(_testRefreshToken.Code));
+            Assert.That(refreshToken.User, Is.EqualTo(_testUser));
+        });
+    }
+
+    [Test]
+    public void CanRecordUsesOfToken()
+    {
+        RefreshToken token = _refreshTokenService.RecordUse(_testRefreshToken).Result;
+        Assert.That(token.Uses, Is.EqualTo(1));
+    }
+
+    [Test]
+    public void CanRecordMultipleUsesOfToken()
+    {
+        _testRefreshToken.Uses = 0;
+        _context.Update(_testRefreshToken);
+        _context.SaveChanges();
+        
+        _testRefreshToken = _refreshTokenService.RecordUse(_testRefreshToken).Result;
+        Assert.That(_testRefreshToken.Uses, Is.EqualTo(1));
+        
+        _testRefreshToken = _refreshTokenService.RecordUse(_testRefreshToken).Result;
+        Assert.That(_testRefreshToken.Uses, Is.EqualTo(2));
+        
+        _testRefreshToken = _refreshTokenService.RecordUse(_testRefreshToken).Result;
+        Assert.That(_testRefreshToken.Uses, Is.EqualTo(3));
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        _context.Dispose();
+    }
+}
---
+++ OIDC.Core-Minimal-Test/TestUtil/RefreshTokenGenerator.cs
@@ -0,0 +1,24 @@
+using OIDC.Core_Minimal.DAL.Entities;
+
+namespace OIDC.Core_Minimal_Test.TestUtil;
+
+public class RefreshTokenGenerator
+{
+    public static Guid FixedGuid() => Guid.Parse("58985300-b353-4346-9a6e-2d2f0c81b9dd");
+
+    public static string FixedCode() =>
+        "d+aGwHsPI9RpWaVoP4UFfsT3EjytsMBKUbrbj1KC9GsswzNe03GQVt3zNWci9ldY3SldtG7DRdQ+l7BHHmtKvA==";
+
+    public static RefreshToken GenerateFixed()
+    {
+        User user = UserGenerator.GenerateFixed();
+        
+        return new RefreshToken
+        {
+            Id = FixedGuid(),
+            UserId = user.Id,
+            Code = FixedCode(),
+            Uses = 0
+        };
+    }
+}
